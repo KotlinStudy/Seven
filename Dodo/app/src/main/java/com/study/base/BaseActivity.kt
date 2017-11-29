1fa67d3f45d13798955f67f75e84145f21b9eff3
zhaijinlu <1375716717@qq.com>
2017-11-29 13:36:18 +0800

    修改冷启动

--- Dodo/app/src/main/java/com/study/base/BaseActivity.kt
+++ Dodo/app/src/main/java/com/study/base/BaseActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.WindowManager
 import android.widget.Toast
+import com.study.R
 
 /**
  *类的用途：
@@ -19,6 +20,7 @@ abstract  class BaseActivity<V ,P:BasePresenter<V>>: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(setLayout())
         //沉浸式状态栏  4.4以上系统
         //通知栏（notification）背景颜色跟随app导航栏（top title）背景颜色变化而变化。
