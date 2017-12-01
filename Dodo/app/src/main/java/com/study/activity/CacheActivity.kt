647dcba82114e5730d6f364eed00235ea12057d4
Zhang010 <1343507214@qq.com>
2017-12-01 15:23:57 +0800

    修改

--- Dodo/app/src/main/java/com/study/activity/CacheActivity.kt
+++ Dodo/app/src/main/java/com/study/activity/CacheActivity.kt
@@ -9,28 +9,13 @@ import android.view.View
 import android.widget.ImageView
 import com.study.R
 import com.study.adapter.CacheAdapter
-import com.study.bean.Video
-import org.greenrobot.eventbus.EventBus
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
+
 
 class CacheActivity : AppCompatActivity() {
-
-    val num : MutableList<Video>? = null
-    //订阅方法
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onEvent(messageEvent: Video){
-        Log.e("ddd","213")
-       // num?.add(messageEvent)
-        Log.e("ddd",messageEvent.title)
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cache)
-        //注册EventBus
-        EventBus.getDefault().register(this)
 
         val cache_back = findViewById(R.id.cache_back) as ImageView
         val cache_lv = findViewById(R.id.recy_cache) as RecyclerView
@@ -38,14 +23,7 @@ class CacheActivity : AppCompatActivity() {
         cache_back.setOnClickListener(View.OnClickListener {
             this.finish()
         })
-
-        var cacheAdapter = CacheAdapter(this,num)
-        cache_lv.adapter = cacheAdapter
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        EventBus.getDefault().unregister(this)
-    }
 
 }
